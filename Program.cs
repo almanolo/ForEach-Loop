a3695c361c334cc861523d6e81aa130a69a7d8f8
Alexandru Lucian Manolache <xdflive@outlook.com>
2020-05-18 06:41:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForEach_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
